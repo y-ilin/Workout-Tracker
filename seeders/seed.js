34484c80c1e5a09e18b76f147e3146852733d0c0
Yi Lin Lee <yilinlee19@gmail.com>
2020-09-05 16:21:55 +1000

    change env variables for seed file

--- seeders/seed.js
+++ seeders/seed.js
@@ -2,10 +2,19 @@ let mongoose = require("mongoose");
 // let db = require("../models");
 const Workout = require("../models/Workout.js");
 
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect("mongodb://localhost/workout", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workout",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  });
 
 let workoutSeed = [
   {
